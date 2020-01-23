ca28ee747ca6b4c575fa7d626d895ba5772e2ff8
marcosmatheusdeandrade <mmatheusandrade2013@gmail.com>
2020-01-22 23:51:02 -0300

    mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react'
-import {View} from 'react-native'
-import {StyleSheet} from 'react-native'
-import MapView from 'react-native-maps'
+import {View, Text} from 'react-native'
+import {StyleSheet, Image} from 'react-native'
+import MapView, {Marker, Callout} from 'react-native-maps'
 import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location'
 
-function Main() {
+function Main({ navigation }) {
 
     const [currentRegion, setCurrentRegion] = useState(null);
 
@@ -36,7 +36,41 @@ function Main() {
         return null;
     }
 
-    return <MapView initialRegion={currentRegion} style={{flex: 1}} />
+    return <MapView initialRegion={currentRegion} style={{flex: 1}}>
+
+        <Marker coordinate={{latitude: -27.211164, longitude: -49.6374491}}>
+        
+            <Image style={styles.avatar} source={{uri: 'https://www.designerd.com.br/wp-content/uploads/2019/04/imagens-blogs-chamar-atencao-publico-3.jpg'}} />
+
+            <Callout onPress={() => {
+                navigation.navigate('Profile', {github_username: 'joao'})
+            }}>
+                <View>
+                    <Text>Marcos Matheus</Text>
+                </View>
+            </Callout>
+        </Marker>
+
+    </MapView>
 }
+
+const styles = StyleSheet.create({
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 180,
+        borderWidth: 4
+    },
+
+    callout: {
+        width: 260
+    },
+
+    devName: {
+        
+    }
+    
+});
 
 export default Main;
--- mobile/src/pages/Profile.js
+++ mobile/src/pages/Profile.js
@@ -1,5 +1,12 @@
-function Profile() {
-    return <View />
+import React, {useState, useEffect} from 'react'
+import {View, Text} from 'react-native'
+import {WebView} from 'react-native-webview'
+
+
+function Profile({navigation}) {
+    const github_username = navigation.getParam('github_username') 
+
+    return <WebView style={{flex: 1}} source={{uri: 'https://github.com/marcosmatheusdeandrade'}} />
 }
 
 export default Profile;
